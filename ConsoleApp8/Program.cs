a0a04c7ec248a1c599c2f4457d4fd8e69de269c0
PhuCuong1999 <50348385+PhuCuong1999@users.noreply.github.com>
2020-05-22 14:33:04 +0700

    TimeInOrder

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -60,32 +60,63 @@ namespace ConsoleApp8
         public static Random gen = new Random();
         public static DateTime RandomDay()
         {
-            //DateTime start = new DateTime(2019, 1, 1);
-            //int range = (DateTime.Today - start).Days;  
-            //return start.AddDays(gen.Next(range));
-            Random rand = new Random();
-            //Note that Random.Next(int, int) is inclusive lower bound, exclusive upper bound
+             Console.OutputEncoding = Encoding.UTF8;
+            // Biến begin lưu ngày bắt đầu là  ngày 1 tháng 1 năm 2018
+            DateTime begin = new DateTime(2018, 1, 1);
+            Random random = new Random();
+            int num = 0;
+            // NGÀY ĐẶT
+            DateTime ngayDat = new DateTime();
+            int gio, phut; // lưu giờ và phút của ngayDat
+            // NGÀY XÁC NHẬN
+            DateTime ngayXacNhan = new DateTime();
+            int gioXacNhan, phutXacNhan; // lưu giờ và phút của ngayXacNhan
+            // NGÀY GIAO CHO ĐƠN VỊ VẬN CHUYỂN
+            DateTime ngayGiaoDVVC = new DateTime();
+            int gioGiaoDVVC, phutGiaoDVVC; // lưu giờ và phút của ngayGiaoDVVC
+            // NGÀY KHÁCH HÀNG NHẬN ĐƯỢC HÀNG
+            DateTime ngayNhanHang = new DateTime();
+            int gioNhanHang, phutNhanHang; // lưu giờ và phút của ngayNhanHang
+            for (int i = 0; i < 1000; i++)
+            {
+                // random ra ngày đặt hàng
+                num = random.Next(801);
+                ngayDat = begin.AddDays(num);
+                // random ra giờ, phút đặt hàng
+                gio = random.Next(24); // chạy từ 0 đến 23
+                phut = random.Next(60); // chạy từ 0 đến 59
+                ngayDat = ngayDat.AddHours(gio);
+                ngayDat = ngayDat.AddMinutes(phut);
 
-                DateTime myDateTime = new DateTime(2018, 1, 1,
-                rand.Next(7, 11), rand.Next(0, 60), 0);
-                    
-            
+                // random ra giờ và phút ngày xác nhận ( = ngày đặt  + (randrom trong 1-10 tiếng))
+                gioXacNhan = random.Next(10);// từ 0 đến 9 giờ
+                phutXacNhan = random.Next(60); // từ 0 đến 59
+                // ngày xác nhận
+                ngayXacNhan = ngayDat.AddHours(gioXacNhan).AddMinutes(phutXacNhan);
 
-            return myDateTime;
+                // random ra ngày giao cho đơn vị vận chuyển = ngày xác nhận  + (randrom trong 10-30 tiếng).
+                gioGiaoDVVC = random.Next(10, 30);// từ 10 đến 29
+                phutGiaoDVVC = random.Next(60); // từ 0 đến 60
 
+                ngayGiaoDVVC = ngayXacNhan.AddHours(gioGiaoDVVC).AddMinutes(phutGiaoDVVC);
+
+                // random ra ngày khách hàng nhận hàng = ngày giao cho đơn vị vận chuyển + (randrom trong 48 - 60 tiếng).
+                gioNhanHang = random.Next(48, 60);// từ 48 đến 59
+                phutNhanHang = random.Next(60); // từ 0 đến 60
+
+                ngayNhanHang = ngayGiaoDVVC.AddHours(gioNhanHang).AddMinutes(phutNhanHang);
+
+                string s= String.Format(" Ngày Đặt : {0,10}\n Ngày Xác Nhận: {1}\n Ngày Giao DVVC: {2}\n Ngày Nhận: {3}\n" +
+                    "===================================\n",
+                    ngayDat, ngayXacNhan, ngayGiaoDVVC, ngayNhanHang);
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
 
 
 
         }
-        // Sau đây sẽ tạo ra một xung đột khi hai người cùng commit file lên.
-        public static DateTime xungDotCuongVuiSuaChoNoXungDot()
-        {
-            DateTime start = new DateTime(2019, 1, 1);
-            int range = (DateTime.Today - start).Days;
-            return start.AddDays(gen.Next(range));
-        }
-        // Giờ qua máy t để check in lên 
-        // Giờ khi mình tải code khi t commit lên nên nó bị xung đột, và nó sẽ bắt mình gộp code
+        
 
 
 
